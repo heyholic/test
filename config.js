fa33965311b9a0951d78198d2beeccda525f2783
heyholic <kimgs444@gmail.com>
2026-05-14 18:16:40 +0900

    분리 수정

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://jaqwphkqtuahfrdxaemu.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphcXdwaGtxdHVhaGZyZHhhZW11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MTc5NTcsImV4cCI6MjA5NDI5Mzk1N30.J_xHyh135Kk-1RbP86N43NgeUSJPmJTXiIZ-a4gf15o'
+};
